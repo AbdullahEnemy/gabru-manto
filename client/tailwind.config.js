b6ffe8388681cdc50a8ce94509f398a2f6cd6117
Sadeem Sajid <60297941+SadeemSajid@users.noreply.github.com>
2024-03-15 16:26:22 +0500

    feat:daisyui (#1)
    Co-authored-by: Sadeem Sajid <sadeem@KALAMEET.Home>

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark']
+  }
+}
+
