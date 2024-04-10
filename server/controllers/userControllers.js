6e518a53a939cc4c504d3bfad8c6e2f88a52f481
AbdullahEnemy <akbuddy8@gmail.com>
2024-04-11 02:31:33 +0500

    changePassword Added and corrected

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -55,18 +55,19 @@ const login = async (req, res, next) => {
 
 const changePassword = async (req, res) => {
   try {
-    if (authurize_user(req, res)) return res;
-
     const existingUser = await User.findOne({
       _id: req.params.id,
     });
     if (!existingUser) {
       return res.status(404).json({ message: "User not found" });
     }
-    const auth = await bcrypt.compare(req.body.currentPassword, existingUser.password);
-   if (!auth) {
-        return res.status(401).json({ message: "Current password is incorrect" });
-   }
+    const auth = await bcrypt.compare(
+      req.body.currentPassword,
+      existingUser.password
+    );
+    if (!auth) {
+      return res.status(401).json({ message: "Current password is incorrect" });
+    }
     existingUser.password = req.body.newPassword;
     await existingUser.save();
     res.status(200).json({ message: "Password updated successfully" });
@@ -78,7 +79,7 @@ const changePassword = async (req, res) => {
 };
 
 const authurize_user = (req, res) => {
-  if (req.user && req.user.userType !== "admin")
+  if (req.user && req.user.userType == "user")
     return res.json({
       status: 403,
       message: "You are not allowed to perform this action",
