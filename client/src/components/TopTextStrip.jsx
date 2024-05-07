27fee3dd8a5bdb3525e85cf24dcd8b366cda57c7
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-05-07 20:17:26 +0500

    feat: scrolling text & routes

---
+++ client/src/components/TopTextStrip.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function TopTextStrip() {
+  return (
+    <div className="w-screen h-8 bg-accent flex items-center">
+      <p className="scrolling-text">
+        ✦ GABRU & MANTO FOOD JOINTS ✦ GABRU & MANTO FOOD JOINTS ✦ GABRU & MANTO
+        FOOD JOINTS ✦ GABRU & MANTO FOOD JOINTS ✦ GABRU & MANTO FOOD JOINTS ✦
+        GABRU & MANTO FOOD JOINTS ✦
+      </p>
+    </div>
+  );
+}
+
+export default TopTextStrip;
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopTextStrip from "../components/TopTextStrip";
+
+function Home() {
+  return (
+    <>
+      <TopTextStrip />
+      <h1 className="text-2xl">Bruh</h1>
+    </>
+  );
+}
+
+export default Home;
