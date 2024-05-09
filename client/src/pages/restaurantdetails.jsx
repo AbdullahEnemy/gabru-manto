8cab71024b4831bb6dcff18e5d404ac1b106113b
Ahmad Ali <ahmadshykh2015@gmail.com>
2024-05-09 20:53:12 +0500

    fix: Fixed Chart Issues

--- client/src/pages/restaurantdetails.jsx
+++ client/src/pages/restaurantdetails.jsx
@@ -6,7 +6,7 @@ import DMComponent from "../components/DetailMenuComponent";
 import RestaurantCard from "../components/RestaurantCard";
 import GMChart from "../components/Chart";
 
-function RestaurantDetails({ image, name, details, menu }) {
+function RestaurantDetails({ resPoint, image, name, details, menu }) {
   return (
     <>
       <TopTextStrip />
@@ -15,10 +15,10 @@ function RestaurantDetails({ image, name, details, menu }) {
         <div className="w-5/6 h-5/6 flex justify-between">
           <div className="w-1/2 flex flex-col items-center">
             <div className="">
-              <RestaurantCard image="arcadian-packages-mall.jpg" name={name} />
+              <RestaurantCard image={image} name={name} />
             </div>
-            <div className="max-w-sm max-h-sm w-full h-full">
-              <GMChart></GMChart>
+            <div className="mt-10 max-w-sm w-full p-4">
+              <GMChart restaurantPoint={resPoint}></GMChart>
             </div>
           </div>
           <div className="w-1/2 bg-secondary">
