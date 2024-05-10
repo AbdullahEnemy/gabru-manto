80de81d9d0f69f4aa8f19e6129de62d743470649
Sadeem Sajid <60297941+SadeemSajid@users.noreply.github.com>
2024-05-10 18:19:36 +0500

    feat: home page  (#4)

--- client/src/components/RestaurantCard.jsx
+++ client/src/components/RestaurantCard.jsx
@@ -1,12 +1,25 @@
-import * as React from "react";
+import React from "react";
 
-export default function RestaurantCard({ image, name }) {
+const RestaurantCard = ({ imgUrl, name }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden border border-black	">
-      <img className="w-full" src={image} alt="Sunset in the mountains" />
-      <div className="text-center px-6 py-4 text-primary bg-secondary">
-        <h1 className="text-2xl	p-5">{name}</h1>
+    <div className="flex flex-col w-72 h-56 border-default text-lg font-mono font-bold">
+      <div className="w-full h-32 border-default">
+        <img src={imgUrl} alt="Restaurant" />
+      </div>
+
+      <div className="flex items-center w-full h-12 border-default bg-secondary justify-center text-primary">
+        <p className="text-2xl">{name}</p>
+      </div>
+
+      <div className="flex">
+        <div className="border-default w-full">
+          <button className="btn w-full rounded-none btn-accent text-lg">
+            VISIT
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default RestaurantCard;
---
+++ client/src/components/TopPickCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// TODO: finish this component
+
+const TopPickCard = ({ imgUrl, name, price }) => {
+  return (
+    <div className="flex flex-col w-72 h-56 border-default text-lg font-mono font-bold">
+      <div className="w-full h-32 border-default">
+        <img src={imgUrl} alt="Food" />
+      </div>
+
+      <div className="flex items-center w-full h-12 border-default">
+        <p className="mx-2">{name}</p>
+      </div>
+
+      <div className="flex">
+        <div className="flex items-center w-48 h-full bg-secondary border-default">
+          <p className="mx-2 text-primary">{price} PKR</p>
+        </div>
+        <div className="border-default">
+          <button className="btn w-24 rounded-none btn-accent text-xl">
+            BUY
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopPickCard;
