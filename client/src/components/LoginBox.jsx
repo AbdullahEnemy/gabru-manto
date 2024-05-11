02ce42c7677da6e18cc17f0c7912eb825d34041d
AbdullahEnemy <akbuddy8@gmail.com>
2024-05-12 02:18:51 +0500

    login page connected to backend

--- client/src/components/LoginBox.jsx
+++ client/src/components/LoginBox.jsx
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const LoginBox = () => {
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:4000/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const res = await response.json();
+
+      console.log(res.status);
+
+      if (response.ok) {
+        window.localStorage.setItem("token", res.token);
+        window.localStorage.setItem("role", res.data.userType);
+        window.localStorage.setItem("loggedIn", true);
+
+        window.location.href = "/";
+      } else {
+        alert("Login failed. Please check your credentials.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert("An error occurred during login. Please try again later.");
+    }
+  }
+
   return (
     <div className="flex justify-center items-center h-screen m-2">
       <div className="hero w-3/4 py-8 bordered-shadow">
         <div className="hero-content flex-col lg:flex-row-reverse">
           {/* form element*/}
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-secondary ml-12">
-            <form className="card-body">
-              
-              {/* username */}
+            <form className="card-body" onSubmit={handleSubmit}>
+              {/* email */}
               <div className="form-control">
                 <div className="label">
-                  <h1 className="text-slate-200 label-text">Username</h1>
+                  <h1 className="text-slate-200 label-text">Email</h1>
                 </div>
-                <label class="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    class="w-4 h-4 opacity-70"
-                  >
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                  </svg>
-                  <input type="text" class="grow" placeholder="Username" />
+                <label className="input input-bordered flex items-center gap-2">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </label>
                 <div className="label">
                   <h1 className="text-slate-200 label-text-alt">
-                    Enter your username
+                    Enter your email
                   </h1>
                 </div>
               </div>
@@ -40,30 +71,26 @@ const LoginBox = () => {
                   <h1 className="text-slate-200 label-text">Password</h1>
                 </div>
                 <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input type="password" className="grow" />
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </label>
                 <div className="label">
                   <h1 className="text-slate-200 label-text-alt">
-                    Enter your password 
+                    Enter your password
                   </h1>
                 </div>
               </div>
 
-              {/* Sign-up button */}
+              {/* Sign-in button */}
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Sign-in</button>
+                <button type="submit" className="btn btn-primary">
+                  Sign-in
+                </button>
               </div>
             </form>
           </div>
@@ -74,8 +101,8 @@ const LoginBox = () => {
               <h1 className="text-[128px] py-8">G&M</h1>
             </div>
             <h1 className="font-medium py-6">
-              Sign-in to continue the <br/>
-              foody exploratin you <br />
+              Sign-in to continue the <br />
+              food exploration you <br />
               left off from!
             </h1>
 
@@ -85,13 +112,11 @@ const LoginBox = () => {
               an account?
             </h1>
             <Link to="/signup">
-            <button className="btn btn-secondary px-8">Sign-up</button>
+              <button className="btn btn-secondary px-8">Sign-up</button>
             </Link>
           </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default LoginBox;
+}
